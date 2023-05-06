2bdc151690322fa19009baa537b053a2eb979d9b
ben :) <carmonben@gmail.com>
2023-05-06 16:43:27 +0300

    made the server run on gunicorn and updated the index function

--- server/main.py
+++
@@ -1,9 +0,0 @@
-from app import app
-import os
-#from pyngrok import ngrok
-
-if __name__ == "__main__":
-    #ngrok_tunnel = ngrok.connect(6969)
-    #print('Public URL:', ngrok_tunnel.public_url)
-
-    app.run("0.0.0.0", port=os.getenv('PORT', 6969))
