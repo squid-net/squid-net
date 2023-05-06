b68fd4b70b8cf70ee81b873b0423d7124d842129
ben :) <carmonben@gmail.com>
2023-05-06 16:43:27 +0300

    made the server run on gunicorn and updated the index function

--- server/app.py
+++ server/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, request
+from flask_socketio import SocketIO, emit
 import redis
 import json
+import os
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 @app.route('/')
 @app.route('/<name>')
-def hello(name):
+def index(name):
     ip_address = request.remote_addr
     user_data = {
         'name': name,
@@ -15,3 +18,7 @@ def hello(name):
     }
     redis_client.publish('names', json.dumps(user_data))
     return f"Hello {name}!"
+
+if __name__ == "__main__":
+
+    socketio.run(app, host = 'localhost', port = 6969)
