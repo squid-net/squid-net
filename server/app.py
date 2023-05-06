d3b38d0fbfe2a26260872e0c0b1e4d04aa337f5e
ben :) <carmonben@gmail.com>
2023-05-06 12:21:11 +0300

    added redis and docker

--- server/app.py
+++ server/app.py
@@ -1,4 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+import redis
+import json
 
 app = Flask(__name__)
-import modules
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
+@app.route('/')
+@app.route('/<name>')
+def hello(name):
+    ip_address = request.remote_addr
+    user_data = {
+        'name': name,
+        'ip': ip_address
+    }
+    redis_client.publish('names', json.dumps(user_data))
+    return f"Hello {name}!"
