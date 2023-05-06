83936cc04ecaecce93215299c572075fe237f567
ben :) <carmonben@gmail.com>
2023-05-06 20:58:42 +0300

    tried using a stream. still doesnt work

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, Response
 from flask_socketio import SocketIO, emit
 import redis
 import json
@@ -9,10 +9,22 @@ socketio = SocketIO(app)
 REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
 REDIS_PORT = os.environ.get('REDIS_PORT', '6379')
 redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+redis_pubsub = redis_client.pubsub()
+redis_pubsub.subscribe('names')
+
 
 @app.route('/admin')
 def admin():
     return app.send_static_file('admin.html')
+
+@app.route('/stream')
+def stream():
+    return Response(event_stream(), mimetype="text/event-stream")
+
+def event_stream():
+
+    for message in redis_pubsub.listen():
+        yield f"data: {message['data']}"
 
 @app.route('/')
 @app.route('/<name>')
@@ -25,26 +37,8 @@ def index(name='anonymous'):
     redis_client.publish('names', json.dumps(user_data))
 
     response = make_response(f"Hello {name}!")
-
     return response
 
-@socketio.on('connect', namespace='/admin')
-def admin_connect():
-    print('Client connected to /admin namespace')
-
-@socketio.on('disconnect', namespace='/admin')
-def admin_disconnect():
-    print('Client disconnected from /admin namespace')
-
-def redis_listener():
-    print("Redis listener started")
-    pubsub = redis_client.pubsub()
-    pubsub.subscribe('names')
-    for message in pubsub.listen():
-        if message['type'] == 'message':
-            name = json.loads(message['data'])['name']
-            socketio.emit('new_name', name, namespace='/admin', broadcast=True)
 
 if __name__ == "__main__":
-    socketio.start_background_task(redis_listener)
-    socketio.run(app, host = 'localhost', port = 6969)
+    socketio.run(app, host = 'localhost', port = 6969, debug=True)
